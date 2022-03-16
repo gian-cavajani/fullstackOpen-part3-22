2f2287326fb6696c9b6dba1c4dcfeb9adc2a049c
Gianluca Cavajani <cavajanig@gmail.com>
2022-03-16 16:56:19 -0300

    ex 3.13

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-// mongodb+srv://gian:<password>@cluster0.sbqjy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
 const app = express();
+const cors = require("cors");
 
+require("dotenv").config();
+const Person = require("./models/person");
+
+const morgan = require("morgan");
 app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
@@ -16,47 +18,28 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let data = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-  {
-    id: 5,
-    name: "a",
-    number: "39-23-6423122",
-  },
-];
-
 //--------------GET------------------------------------
 
 app.get("/api/persons", (req, res) => {
-  res.json(data);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = data.find((p) => p.id === id);
-  if (!person) {
-    res.status(404).send("404 person not found");
-  }
-  res.json(person);
+  // const id = Number(req.params.id);
+  // const person = data.find((p) => p.id === id);
+  // if (!person) {
+  //   res.status(404).send("404 person not found");
+  // }
+  // res.json(person);
+  Person.findById(req.params.id)
+    .then((person) => {
+      res.json(person);
+    })
+    .catch((err) => {
+      res.status(404).send("404 person not found");
+    });
 });
 
 app.get("/info", (req, res) => {
