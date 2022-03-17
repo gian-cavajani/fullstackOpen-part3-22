3174dc4d4d442ea2d6c639f4059da74244cb03b3
Gianluca Cavajani <cavajanig@gmail.com>
2022-03-17 12:19:23 -0300

    ex 3.16

--- index.js
+++ index.js
@@ -14,33 +14,47 @@ morgan.token("body", (req, res) => {
   return JSON.stringify(req.body);
 });
 
+const errorHandler = (error, req, res, next) => {
+  console.log(error);
+  return res.status(404).send({ error: error });
+  next(error);
+};
+
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
 //--------------GET------------------------------------
 
-app.get("/api/persons", (req, res) => {
-  Person.find({}).then((persons) => {
-    res.json(persons);
-  });
+app.get("/api/persons", (req, res, next) => {
+  Person.find({})
+    .then((persons) => {
+      res.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   Person.findById(req.params.id)
     .then((person) => {
       res.json(person);
     })
     .catch((err) => {
-      res.status(404).send("404 person not found");
+      next(err);
     });
 });
 
 app.get("/info", (req, res) => {
-  const peopleCount = data.length;
-  res.send(
-    `<p>phonebook has info for ${peopleCount} people</p><p>${new Date()}</p>`
-  );
+  Person.find({})
+    .then((persons) => {
+      console.log(persons.length);
+      res.send(
+        `<p>phonebook has info for ${
+          persons.length
+        } people</p><p>${new Date()}</p>`
+      );
+    })
+    .catch((error) => next(error));
 });
 
 //--------------DELETE------------------------------------
@@ -49,29 +63,31 @@ app.delete("/api/persons/:id", (req, res) => {
   Person.findByIdAndDelete(req.params.id)
     .then(res.status(204).send({}))
     .catch((err) => {
-      console.log(err);
+      next(err);
     });
 });
 
 //--------------POST------------------------------------
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const body = req.body;
   if (!body.number || !body.name) {
     return res.status(400).json({ error: "name or number is missing" }).end();
   }
-  // if (data.some((p) => p.name.toUpperCase() == body.name.toUpperCase())) {
-  //   return res.status(400).json({ error: "name repeated" }).end();
-  // }
+
   const newPerson = new Person({
-    // id: Math.random() * 100,
     name: body.name,
     number: body.number,
   });
-  newPerson.save().then((savedPerson) => {
-    res.json(savedPerson);
-  });
+
+  newPerson
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
+app.use(errorHandler);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {});
